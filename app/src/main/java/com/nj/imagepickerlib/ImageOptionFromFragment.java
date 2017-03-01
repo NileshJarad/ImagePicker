0d60b88a71908c1cc85a0ab78e97ed10db43ead7
Nilesh Jarad <nlshjarad7@gmail.com>
2017-03-01 12:22:08 +0530

    Updated readme file. Added release implementation info for V1.0.3.

--- app/src/main/java/com/nj/imagepickerlib/ImageOptionFromFragment.java
+++ app/src/main/java/com/nj/imagepickerlib/ImageOptionFromFragment.java
@@ -1,10 +1,12 @@
 package com.nj.imagepickerlib;
 
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,8 @@ import com.nj.imagepicker.utils.DialogConfiguration;
  * A simple {@link Fragment} subclass.
  */
 public class ImageOptionFromFragment extends Fragment {
+
+private static final String LOG_TAG = "ImageOptionFromFragment";
 
 
     private ImageView ivImage;
@@ -51,10 +55,15 @@ public class ImageOptionFromFragment extends Fragment {
     public void show() {
         ImagePicker.build(new DialogConfiguration()
                 .setTitle("Choose")
+                .setResultImageDimension(200,200)
                 .setOptionOrientation(LinearLayoutCompat.HORIZONTAL), new ImageResultListener() {
             @Override
             public void onImageResult(ImageResult imageResult) {
-                ivImage.setImageBitmap(imageResult.getBitmap());
+                Bitmap bitmap = imageResult.getBitmap();
+
+                Log.e(LOG_TAG, "onImageResult: "+bitmap.getWidth());
+
+                ivImage.setImageBitmap(bitmap);
             }
         }).show(getChildFragmentManager());
     }
